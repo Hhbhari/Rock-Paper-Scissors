e61661f68c9e9a121e464f88b49246d71e3c6680
Hhbhari <159613769+Hhbhari@users.noreply.github.com>
2024-02-18 21:21:31 +0530

    feat: add new logic on game
    Introduced new logic for playing the game five times and calculating the points overall. Display the winner or loser based on the points at the end.

--- script.js
+++ script.js
@@ -1,32 +1,62 @@
-const playerSelection=prompt("Rock, Paper or Scissors?");
-const computerSelection=getComputerchoice();
+const start=0;
+let win=0
 
-console.log(playRound(playerSelection,computerSelection));
+for(let i=0; i<5; i++){
+    playGame();
+}
+console.log(checkWinner(win));
+console.log(win);
 
-function playRound(playerSelection, computerSelection){
+function playGame(){
+    const playerSelection=prompt("Rock, Paper or Scissors?");
+    const computerSelection=getComputerchoice();
+
+    win += playRound(playerSelection, computerSelection, start);
+}
+
+function playRound(playerSelection, computerSelection, start){
     if(playerSelection.toLowerCase()===computerSelection.toLowerCase()){
-        return `Sorry!, It's a tie`;
+        console.log( "Sorry!, It's a tie");
+        return 0;
     }
     else if(playerSelection.toLowerCase()==='rock' && computerSelection.toLowerCase()==='scissors'){
-        return `You Win! Rock beats Scissors`;
+        console.log( "You Win! Rock beats Scissors");
+        return 1;
     }
     else if(playerSelection.toLowerCase()==='rock' && computerSelection.toLowerCase()==='paper'){
-        return `You Lose! Paper beats Rock`;
+        console.log( "You Lose! Paper beats Rock");
+        return -1;
     }
     else if(playerSelection.toLowerCase()==='scissors' && computerSelection.toLowerCase()==='rock'){
-        return `You Lose! Rock beats Scissors`;
+        console.log( "You Lose! Rock beats Scissors");
+        return -1;
     }
     else if(playerSelection.toLowerCase()==='scissors' && computerSelection.toLowerCase()==='paper'){
-        return `You Win! Scissors beats Paper`;
+        console.log( "You Win! Scissors beats Paper");
+        return 1;
     }
     else if(playerSelection.toLowerCase()==='paper' && computerSelection.toLowerCase()==='scissors'){
-        return `You Lose! Scissors beats Paper`;
+        console.log( "You Lose! Scissors beats Paper");
+        return -1;
     }
     else if(playerSelection.toLowerCase()==='paper' && computerSelection.toLowerCase()==='rock'){
-        return `You Win! Paper beats Rock`;
+        console.log( "You Win! Paper beats Rock");
+        return +1;
     }
     else {
-        return `Wrong Input`;
+        console.log( "Wrong Input");
+    }
+}
+
+function checkWinner(win){
+    if(win<0){
+        return `Sorry!, Computer is the Winner`;
+    }
+    else if(win>0){
+        return `Hurray!, You are the Winner`;
+    }
+    else{
+        return `It's a tie.`
     }
 }
 
