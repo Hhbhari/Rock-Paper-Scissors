160a7c6d98c8addb448569043cc8e2ddbbff62b2
Hhbhari <159613769+Hhbhari@users.noreply.github.com>
2024-02-18 19:53:43 +0530

    fix: fix logic on script

--- script.js
+++ script.js
@@ -10,20 +10,20 @@ function playRound(playerSelection, computerSelection){
     else if(playerSelection.toLowerCase()==='rock' && computerSelection.toLowerCase()==='scissors'){
         return `You Win! Rock beats Scissors`;
     }
-    else if(playerSelection.toLowerCase()=='scissors' && computerSelection.toLowerCase()==='rock'){
+    else if(playerSelection.toLowerCase()==='rock' && computerSelection.toLowerCase()==='paper'){
+        return `You Lose! Paper beats Rock`;
+    }
+    else if(playerSelection.toLowerCase()==='scissors' && computerSelection.toLowerCase()==='rock'){
         return `You Lose! Rock beats Scissors`;
     }
-    else if(playerSelection.toLowerCase()=='paper' && computerSelection.toLowerCase()==='scissors'){
-        return `You Lose! Scissors beats Paper`;
-    }
-    else if(playerSelection.toLowerCase()=='scissors' && computerSelection.toLowerCase()==='paper'){
+    else if(playerSelection.toLowerCase()==='scissors' && computerSelection.toLowerCase()==='paper'){
         return `You Win! Scissors beats Paper`;
     }
-    else if(playerSelection.toLowerCase()=='rock' && computerSelection.toLowerCase()==='paper'){
-        return `You Lose! Rock beats Paper`;
+    else if(playerSelection.toLowerCase()==='paper' && computerSelection.toLowerCase()==='scissors'){
+        return `You Lose! Scissors beats Paper`;
     }
-    else if(playerSelection.toLowerCase()=='paper' && computerSelection.toLowerCase()==='rock'){
-        return `You Win! Rock beats Paper`;
+    else if(playerSelection.toLowerCase()==='paper' && computerSelection.toLowerCase()==='rock'){
+        return `You Win! Paper beats Rock`;
     }
     else {
         return `Wrong Input`;
